78b2d37168e19f51f414f2607bda343309bb5884
BBishop <brad.bishop68@gmail.com>
2016-01-20 15:57:16 -1000

    Finishes 2nd timing exercise

--- indexing.sql
+++ indexing.sql
@@ -3,4 +3,23 @@ CREATE DATABASE indexed_cars;
 ALTER DATABASE indexed_cars OWNER TO inexed_cars_user;
 
 \c indexed_cars;
-\i /scripts/car_models.sql;
+\i scripts/car_models.sql;
+
+\i scripts/car_model_data.sql
+\i scripts/car_model_data.sql
+\i scripts/car_model_data.sql
+\i scripts/car_model_data.sql
+\i scripts/car_model_data.sql
+\i scripts/car_model_data.sql
+\i scripts/car_model_data.sql
+\i scripts/car_model_data.sql
+\i scripts/car_model_data.sql
+\i scripts/car_model_data.sql
+-- Turns on timing ot time statements
+
+\timing
+SELECT DISTINCT make_title FROM car_models WHERE make_code LIKE '%LAM%';
+-- timing took 24.363 ms
+
+SELECT DISTINCT model_title FROM car_models WHERE make_code= 'NISSAN' AND model_code= 'GT-R';
+-- timing took 21.356 ms
